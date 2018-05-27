a39658e3ce7959ba147f45d3ff70b52ea39c3715
Dennis Tijbosch <d.tijbosch@yahoo.com>
2018-05-27 14:00:25 +0200

    Added a LineReader and CircuitBuilder class with read options

---
+++ CircuitBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircuitMagieDeluxe
+{
+    class CircuitBuilder
+    {
+        public CircuitBuilder()
+        {
+
+        }
+
+        public string ReadFile(String path)
+        {
+            string text;
+            FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
+
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                text = streamReader.ReadToEnd();
+            }
+
+            return text;
+        }
+    }
+}
---
+++ Helpers/LineReader.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CircuitMagieDeluxe.Helpers
+{
+    public sealed class LineReader : IEnumerable<string>
+    {
+        readonly Func<TextReader> dataSource;
+
+        // LineReader maken op basis van de streamSource (UTF8 default)
+        public LineReader(Func<Stream> streamSource)
+            : this(streamSource, Encoding.UTF8)
+        {
+        }
+
+        // LineReader maken op basis van de streamSource (custom encoding)
+        public LineReader(Func<Stream> streamSource, Encoding encoding)
+            : this(() => new StreamReader(streamSource(), encoding))
+        {
+        }
+
+        // LineReader maken op basis van filename (UTF8 default)
+        public LineReader(string filename)
+            : this(filename, Encoding.UTF8)
+        {
+        }
+
+        // LineReader maken op basis van filename (custom encoding)
+        public LineReader(string filename, Encoding encoding)
+            : this(() => new StreamReader(filename, encoding))
+        {
+        }
+
+        // LineReader maken op basis van de textReaderSource
+        public LineReader(Func<TextReader> dataSource)
+        {
+            this.dataSource = dataSource;
+        }
+
+        // Loop door alle lines heen
+        public IEnumerator<string> GetEnumerator()
+        {
+            using (TextReader reader = dataSource())
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    yield return line;
+                }
+            }
+        }
+
+        // Roep enumerator aan om loop te starten
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
