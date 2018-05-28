b46ee85d072529b2c165bee34a69e488c8fcc3b2
Dennis Tijbosch <d.tijbosch@yahoo.com>
2018-05-28 21:49:25 +0200

    added class to wirte output to TextBox

---
+++ Helpers/OutputPrinter.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows.Controls;
+
+namespace CircuitMagieDeluxe.Helpers
+{
+    public class OutputPrinter : TextWriter
+    {
+        TextBox output = null; 
+
+        public OutputPrinter(TextBox _output)
+        {
+            output = _output;
+        }
+
+        public override void Write(char value)
+        {
+            base.Write(value);
+            output.AppendText(value.ToString());
+        }
+
+        public override Encoding Encoding
+        {
+            get { return System.Text.Encoding.UTF8; }
+        }
+    }
+}
