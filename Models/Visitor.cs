21a2c2b5fdbb9f042b82065b84e7853e858fa645
Dennis Tijbosch <d.tijbosch@yahoo.com>
2018-06-03 15:34:56 +0200

    cleanup code

--- Models/Visitor.cs
+++ Models/Visitor.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CircuitMagieDeluxe.Models
+﻿namespace CircuitMagieDeluxe.Models
 {
     interface Visitor 
     {
